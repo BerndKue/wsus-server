0d5f905ca4eb5c6a4f8c5f2f017a2d5da82de0de
d032408 <bernd.kuespert@sap.com>
2022-06-21 16:52:04 +0200

    add some debugging output

--- providers/subscription.rb
+++ providers/subscription.rb
@@ -85,6 +85,7 @@ action :configure do
   categories_unchanged = compare_array_and_hash(@new_resource.categories, @category_map)
   classifications_unchanged = compare_array_and_hash(@new_resource.classifications, @classificiation_map)
 
+  Chef::Log.info("Sync Running?: #{sync_running?}")
   unless (updated_properties.empty? && categories_unchanged && classifications_unchanged) || sync_running?
     converge_by 'configuring wsus server subscription' do
       script = <<-EOS
